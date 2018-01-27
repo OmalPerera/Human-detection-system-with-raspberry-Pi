f17f01026c594268b7a7414536983be9a50be7bd
Pratyush Das <reikdas@gmail.com>
2018-01-27 23:21:41 +0530

    Fixed indentation error

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -2,7 +2,7 @@
 # python pi_surveillance.py --conf conf.json
 
 # import the necessary packages
-from pyimagesearch.tempimage import TempImage
+from __future__ import print_function
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import argparse
@@ -13,15 +13,13 @@ import json
 import time
 import cv2
 import RPi.GPIO as GPIO
-from __future__ import print_function
 from imutils.object_detection import non_max_suppression
-from imutils import paths
 import numpy as np
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--conf", required=True,
-	help="path to the JSON configuration file")
+    help="path to the JSON configuration file")
 args = vars(ap.parse_args())
 
 # filter warnings, load the configuration
@@ -54,115 +52,115 @@ motionCounter = 0
 
 # capture frames from the camera
 for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image and initialize
-	# the timestamp and occupied/unoccupied text
-	frame = f.array
-	timestamp = datetime.datetime.now()
-	text = "Unoccupied"
+    # grab the raw NumPy array representing the image and initialize
+    # the timestamp and occupied/unoccupied text
+    frame = f.array
+    timestamp = datetime.datetime.now()
+    text = "Unoccupied"
 
-	# resize the frame
-	frame = imutils.resize(frame, width=500)
+    # resize the frame
+    frame = imutils.resize(frame, width=500)
     orig = frame.copy()
 
     # detect people in the image
-	(rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),
-		padding=(8, 8), scale=1.05)
+    (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),
+        padding=(8, 8), scale=1.05)
 
     # convert image to grayscale and blur it
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray, (21, 21), 0)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-	# if the average frame is None, initialize it
-	if avg is None:
-		print("[INFO] starting background model...")
-		avg = gray.copy().astype("float")
-		rawCapture.truncate(0)
-		continue
+    # if the average frame is None, initialize it
+    if avg is None:
+        print("[INFO] starting background model...")
+        avg = gray.copy().astype("float")
+        rawCapture.truncate(0)
+        continue
 
-	# accumulate the weighted average between the current frame and
-	# previous frames, then compute the difference between the current
-	# frame and running average
-	cv2.accumulateWeighted(gray, avg, 0.5)
-	frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
+    # accumulate the weighted average between the current frame and
+    # previous frames, then compute the difference between the current
+    # frame and running average
+    cv2.accumulateWeighted(gray, avg, 0.5)
+    frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
-	# threshold the delta image, dilate the thresholded image to fill
-	# in holes, then find contours on thresholded image
-	thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
-		cv2.THRESH_BINARY)[1]
-	thresh = cv2.dilate(thresh, None, iterations=2)
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-		cv2.CHAIN_APPROX_SIMPLE)
-	cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    # threshold the delta image, dilate the thresholded image to fill
+    # in holes, then find contours on thresholded image
+    thresh = cv2.threshold(frameDelta, conf["delta_thresh"], 255,
+        cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-	# loop over the contours
-	for c in cnts:
-		# if the contour is too small, ignore it
-		if cv2.contourArea(c) < conf["min_area"]:
-			continue
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < conf["min_area"]:
+            continue
 
-		# draw the original bounding boxes
-	    for (x, y, w, h) in rects:
-		    cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # draw the original bounding boxes
+        for (x, y, w, h) in rects:
+            cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
  
-	    # apply non-maxima suppression to the bounding boxes using a
-	    # fairly large overlap threshold to try to maintain overlapping
-	    # boxes that are still people
-	    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-	    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+        # apply non-maxima suppression to the bounding boxes using a
+        # fairly large overlap threshold to try to maintain overlapping
+        # boxes that are still people
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
  
-	    # draw the final bounding boxes
-	    for (xA, yA, xB, yB) in pick:
-	    	cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+        # draw the final bounding boxes
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 
-	    text = "Occupied"
+        text = "Occupied"
 
-	# draw the text and timestamp on the frame
-	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
-	cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-		0.35, (0, 0, 255), 1)
+    # draw the text and timestamp on the frame
+    ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
+    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+        0.35, (0, 0, 255), 1)
 
-	# check to see if the room is occupied
-	if text == "Occupied":
-		
-		#Activate pin
-		GPIO.output(pin, GPIO.HIGH)
-		
-		# check to see if enough time has passed between uploads
-		if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
-			# increment the motion counter
-			motionCounter += 1
+    # check to see if the room is occupied
+    if text == "Occupied":
+        
+        #Activate pin
+        GPIO.output(pin, GPIO.HIGH)
+        
+        # check to see if enough time has passed between uploads
+        if (timestamp - lastUploaded).seconds >= conf["min_upload_seconds"]:
+            # increment the motion counter
+            motionCounter += 1
 
-			# check to see if the number of frames with consistent motion is
-			# high enough
-			if motionCounter >= conf["min_motion_frames"]:
+            # check to see if the number of frames with consistent motion is
+            # high enough
+            if motionCounter >= conf["min_motion_frames"]:
 
-				# update the last uploaded timestamp and reset the motion
-				# counter
-				lastUploaded = timestamp
-				motionCounter = 0
+                # update the last uploaded timestamp and reset the motion
+                # counter
+                lastUploaded = timestamp
+                motionCounter = 0
 
-	# otherwise, the room is not occupied
-	else:
-		motionCounter = 0
-		
-		#Deactivate Pin
-		GPIO.output(pin, GPIO.LOW)
-		
+    # otherwise, the room is not occupied
+    else:
+        motionCounter = 0
+        
+        #Deactivate Pin
+        GPIO.output(pin, GPIO.LOW)
+        
 
-	# check to see if the frames should be displayed to screen
-	if conf["show_video"]:
-		# display the security feed
-		cv2.imshow("Security Feed", frame)
-		key = cv2.waitKey(1) & 0xFF
+    # check to see if the frames should be displayed to screen
+    if conf["show_video"]:
+        # display the security feed
+        cv2.imshow("Security Feed", frame)
+        key = cv2.waitKey(1) & 0xFF
 
-		# if the `q` key is pressed, break from the lop
-		if key == ord("q"):
-			break
+        # if the `q` key is pressed, break from the lop
+        if key == ord("q"):
+            break
 
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
 
 GPIO.cleanup()
 
